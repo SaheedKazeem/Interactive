71dbf8702303ce24cb49660472a8824fa42eb200
SaheedKazeem <saheekazeem@gmail.com>
2023-07-25 22:24:30 +0100

    hm

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public VideoPlayer videoPlayer;
+    public Button playButton;
+    public Button speedUpButton;
+
+    private bool isPlaying = false;
+    private float defaultPlaybackSpeed;
+    private float speedMultiplier = 2.0f; // You can adjust this value to change the speed increase amount.
+
+    private void Start()
     {
-        
+        videoPlayer.playOnAwake = false;
+        defaultPlaybackSpeed = videoPlayer.playbackSpeed;
+
+        playButton.onClick.AddListener(TogglePlay);
+        speedUpButton.onClick.AddListener(SpeedUp);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TogglePlay()
     {
-        
+        if (isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else
+        {
+            videoPlayer.Play();
+        }
+
+        isPlaying = !isPlaying;
+    }
+
+    private void SpeedUp()
+    {
+        videoPlayer.playbackSpeed *= speedMultiplier;
     }
 }
